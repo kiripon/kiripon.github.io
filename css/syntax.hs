9cd52aa3ce809b4388b975bf4bd800723577e6a2
kiripon <painapoo@gmail.com>
2016-02-13 20:07:11 +0900

    update

---
+++ css/syntax.hs
@@ -0,0 +1,86 @@
+
+@font-face
+{
+  font-family : "Hasklig";
+  src         : local("../fonts/Hasklig-Medium.otf");
+}
+
+pre
+{
+  margin           : 0px 10px 0px 10px;
+  padding          : 9px 9px 9px 9px;
+  background-color : rgb(253,246,227);
+  border           : solid 1px rgba(0,0,0,0.5000);
+  font-family      : "Hasklig","Monaco","Menlo","Consolas","Courier New", monospace;
+  font-size        : 14px;
+  border-radius    : 5px 5px 5px 5px;
+  text-rendering   : optimizeLegibility;
+  white-space      : pre-wrap;
+}
+
+pre code
+{
+  border-style       : none;
+  display            : block;
+  -webkit-box-shadow : none none none none;
+  -moz-box-shadow    : none none none none;
+  -ms-box-shadow     : none none none none;
+  -o-box-shadow      : none none none none;
+  box-shadow         : none none none none;
+}
+
+
+.sourceCode .kw
+{
+  color : rgb(38,139,210);
+}
+
+.sourceCode .dt
+{
+  color : rgb(38,139,210);
+}
+
+.sourceCode .dv,
+.sourceCode .bn,
+.sourceCode .fl
+{
+  color : rgb(211,54,130);
+}
+
+.sourceCode .ch
+{
+  color : rgb(220,50,47);
+}
+
+.sourceCode .st
+{
+  color : rgb(42,161,152);
+}
+
+.sourceCode .co
+{
+  color : rgb(147,161,161);
+}
+
+.sourceCode .ot
+{
+  color : rgb(165,120,0);
+}
+
+.sourceCode .al
+{
+  color : rgb(203,75,22);
+}
+
+.sourceCode .fu
+{
+  color : rgb(38,139,210);
+}
+
+.sourceCode .er
+{
+  color : rgb(211,1,2);
+}
+
+
+/* Generated with Clay, http://fvisser.nl/clay */
